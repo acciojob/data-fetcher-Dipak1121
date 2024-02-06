9410d2e7e1b7ad7fba55704b9f2d2433e12d6b91
Dipak1121 <dipaks9604@gmail.com>
2024-02-06 19:11:14 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+// import axios from "axios";
 
 const App = () => {
+
+  const [data, setData] = useState("");
+
+  const api = "https://dummyjson.com/products";
+
+  function fetchAPI(){
+    fetch(api)
+    .then((response)=>response.json())
+    .then((data)=>{
+      console.log(typeof data);
+      console.log(data);
+      setData(JSON.stringify(data));
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+    
+  }
+
+  useState(()=>{
+    fetchAPI();
+  },[])
+
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <pre>{data}</pre>
     </div>
   )
 }
