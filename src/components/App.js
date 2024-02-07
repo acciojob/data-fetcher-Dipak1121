0d7ec00aed7b8cb27109dd591a9e1f6ed6c74c94
Dipak1121 <dipaks9604@gmail.com>
2024-02-07 10:53:59 +0530

    fifth commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,69 @@
 
-import React, { useState } from "react";
-import './../styles/App.css';
-import axios from "axios";
+// import React, { useState } from "react";
+// import './../styles/App.css';
 // import axios from "axios";
+// // import axios from "axios";
+
+// const App = () => {
+
+//   const [data, setData] = useState("");
+
+//   const api = "https://dummyjson.com/products";
+
+//   function fetchAPI(){
+//     fetch(api)
+//     .then((response)=>response.json())
+//     .then((data)=>{
+//       console.log(typeof data);
+//       console.log(data);
+//       setData(JSON.stringify(data));
+//     })
+//     .catch((err)=>{
+//       console.log(err);
+//     });
+
+    
+    
+//   }
+
+//   useState(()=>{
+//     fetchAPI();
+//   },[])
+
+
+//   return (
+//     <div>
+//         <h1>Data Fetched from API</h1>
+//         <pre>{data}</pre>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "./../styles/App.css";
 
 const App = () => {
+  const [apiFetch, setApiFetch] = useState([]);
 
-  const [data, setData] = useState("");
-
-  const api = "https://dummyjson.com/products";
-
-  function fetchAPI(){
-    fetch(api)
-    .then((response)=>response.json())
-    .then((data)=>{
-      console.log(typeof data);
-      console.log(data);
-      setData(JSON.stringify(data));
-    })
-    .catch((err)=>{
-      console.log(err);
-    });
-
-    
-    
-  }
-
-  useState(()=>{
-    fetchAPI();
-  },[])
-
+  useEffect(() => {
+    Axios.get("https://dummyjson.com/products")
+      .then((res) => setApiFetch(res.data))
+      .catch((err) => setApiFetch(err));
+  }, []);
 
   return (
     <div>
-        <h1>Data Fetched from API</h1>
-        <pre>{data}</pre>
+      <h1>Data Fetched from API</h1>
+      {console.log(apiFetch)}
+      {/* {console.log(apiFetch.products, apiFetch.status)} */}
+      {(!apiFetch.code && <pre>{JSON.stringify(apiFetch, null, 2)}</pre>) ||
+        "An error occurred: "}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
